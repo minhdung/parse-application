7ea43924e00ddf0c58dcc4b1f12f384941f1504f
Roneesh Vashisht <roneesh@gmail.com>
2012-11-29 03:18:17 -0600

    first commit with basic page layout created

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  
+	def landing
+
+	end
+
+	def new
+
+	end
+
+	def json
+
+	end
+
+	def goodbye
+
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParseApplication::Application.config.secret_token = '8be773338678414af4d4553e88fe92bf09a55e97ad5aa053e721ffe7106deae91ad2b5d692b45bfca99920e7f44d9790162a08437a0e5b149f8333487a662563'
